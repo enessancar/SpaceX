23e9e62f5916f08d9a5ccf1e0668ac83ab22ab6a
enessancar <enes57751907@gmail.com>
2023-05-26 19:42:51 +0300

    I added the url links of the data where the data will come from

---
+++ SpaceX/Utilities/Enums/APIURLs.swift
@@ -0,0 +1,19 @@
+//
+//  APIURLs.swift
+//  SpaceX
+//
+//  Created by Enes Sancar on 26.05.2023.
+//
+
+import Foundation
+
+enum APIURLs {
+    private static let baseURL = "https://api.spacexdata.com/v5/launches/"
+    
+    static let upcoming = "\(baseURL)upcoming"
+    static let past = "\(baseURL)past"
+    
+    static func launchDetail(id: String) -> String {
+        "\(baseURL)\(id)"
+    }
+}
