64b6d7e47c9ad95593e311828f7745ab6730d22f
enessancar <enes57751907@gmail.com>
2023-05-26 14:41:09 +0300

    foldering structure has been created in the project full programmatically removed main file to be replaced

--- SpaceX/Core/Screens/HomeScreen/HomeVC.swift
+++ SpaceX/Core/Screens/HomeScreen/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  SpaceX
 //
 //  Created by Enes Sancar on 26.05.2023.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
