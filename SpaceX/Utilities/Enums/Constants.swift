864656f7c290044add3bb525f44b2edee11dcab5
enessancar <enes57751907@gmail.com>
2023-05-26 20:38:21 +0300

    I have added certain constants that will be needed in the application

---
+++ SpaceX/Utilities/Enums/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  SpaceX
+//
+//  Created by Enes Sancar on 26.05.2023.
+//
+
+import UIKit
+
+enum SFSymbols {
+    static let youtube = "play.circle.fill"
+    static let presskit = "paperplane.circle.fill"
+    static let article = "doc.circle.fill"
+    static let wikipedia = "building.columns.circle.fill"
+}
+
+enum Images {
+    static let noImage = UIImage(named: "noImage")
+}
+
+enum ScreenSize {
+    static let width = UIScreen.main.bounds.size.width
+    static let height = UIScreen.main.bounds.size.height
+}
+
