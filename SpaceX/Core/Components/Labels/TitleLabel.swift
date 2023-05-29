fc5c84bf2e308e08a823f5f7da54d410024fec39
enessancar <enes57751907@gmail.com>
2023-05-29 11:59:32 +0300

    I wrote extension for title label

---
+++ SpaceX/Core/Components/Labels/TitleLabel.swift
@@ -0,0 +1,40 @@
+//
+//  TitleLabel.swift
+//  SpaceX
+//
+//  Created by Enes Sancar on 29.05.2023.
+//
+
+import UIKit
+
+final class TitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(text: String, textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        
+        self.text = text
+        self.textAlignment = textAlignment
+        self.font = .boldSystemFont(ofSize: fontSize)
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        numberOfLines = 1
+        textColor = .black
+        
+        adjustsFontForContentSizeCategory = true
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        
+        lineBreakMode = .byTruncatingTail
+    }
+}
