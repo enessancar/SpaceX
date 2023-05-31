c8ea8adfecbe8c69ce8012b328e8dc675b215b00
enessancar <enes57751907@gmail.com>
2023-05-31 14:47:48 +0300

    I created a link to the attributes cell and wrote attributes view controller

--- SpaceX/Core/Components/ViewControllers/AttributesVC.swift
+++ SpaceX/Core/Components/ViewControllers/AttributesVC.swift
@@ -22,12 +22,40 @@ final class AttributesVC: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollectionView()
+    }
+    
     private func configureCollectionView() {
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: UIHelper.createHomeCompositionLayout())
         view.addSubview(collectionView)
         
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        //collectionView.dataSource = self
-        //collectionView
+        collectionView.dataSource = self
+        collectionView.register(AttributeCell.self, forCellWithReuseIdentifier: AttributeCell.identifier)
+        collectionView.isUserInteractionEnabled = false
+        
+        collectionView.backgroundColor = .white.withAlphaComponent(0.95)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension AttributesVC: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AttributeCell.identifier, for: indexPath) as? AttributeCell else {
+            fatalError()
+        }
+        let attribute = launch._attributeNames[indexPath.item]
+        let value = launch._attributeValues[indexPath.item]
+        cell.setup(attribute: attribute, value: value)
+        return cell
     }
 }
